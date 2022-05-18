1cf2451e1bb078594648649903856d7c140cad33
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-05-18 21:00:08 +0530

    Connect MySQL database

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const dotenv = require("dotenv");
-
-require("dotenv").config();
 
 app = express();
 
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const { app } = require("./app");
-const PORT = process.env.PORT || 3001;
+const { connPool } = require("./database/dbConnect");
 
-console.log(PORT);
+require("dotenv").config();
 
-app.listen(PORT, () => {
-  console.log(`Listening on port : ${PORT}`);
+connPool.getConnection(function (err, conn) {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Database connected");
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Listening on port : ${PORT}`);
+  });
 });
