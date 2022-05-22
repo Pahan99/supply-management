df6519f7afe9440448f738850ae16ea8c4fd2030
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-05-22 23:41:07 +0530

    Add database migrations

---
+++ migrations/sqls/20220522172900-roles-up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `roles` (
+	`role_id` INT NOT NULL AUTO_INCREMENT,
+	`role_name` varchar(15) NOT NULL,
+	PRIMARY KEY (`role_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
---
+++ migrations/sqls/20220522173430-users-up.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE `users` (
+	`user_id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255),
+	`password` varchar(255) NOT NULL,
+	`phone` varchar(13) NOT NULL,
+	`role_id` INT NOT NULL,
+	PRIMARY KEY (`user_id`),
+    CONSTRAINT `users_fk0` FOREIGN KEY (`role_id`) REFERENCES `roles`(`role_id`) ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
