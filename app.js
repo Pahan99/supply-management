d7294cac13bc548bce811279d0b8cc2d6405a238
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-05-29 13:17:24 +0530

    Add views

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
+// routes
+const homeRoute = require("./routes/home");
+
 app = express();
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome");
-});
+app.get("/", homeRoute);
 
 exports.app = app;
---
+++ models/User.js
@@ -0,0 +1,5 @@
+const {db,dbConn} = require('../database/db-config')
+
+const save = async (user)=>{
+    
+}
