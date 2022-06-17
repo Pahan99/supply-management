f947c2578fac8bb5a6db566cc8ac195f4cffec02
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-06-17 18:45:44 +0530

    edit order details schema

--- migrations/sqls/20220613060710-order-details-up.sql
+++ migrations/sqls/20220613060710-order-details-up.sql
@@ -2,7 +2,6 @@ CREATE TABLE `order_details` (
 	`order_id` INT NOT NULL,
 	`product_id` INT NOT NULL,
 	`quantity` INT,
-	`unit_price` DECIMAL(10,2),
     CONSTRAINT `order_details_fk0` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`) ON DELETE CASCADE,
     CONSTRAINT `order_details_fk1` FOREIGN KEY (`order_id`) REFERENCES `orders`(`order_id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
