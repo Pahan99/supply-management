927ae63be22ddac215741a37d417548053af0fbe
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-07-04 13:57:10 +0530

    Edit routes table

--- migrations/sqls/20220613042948-routes-up.sql
+++ migrations/sqls/20220613042948-routes-up.sql
@@ -2,5 +2,7 @@ CREATE TABLE `routes` (
 	`route_id` INT NOT NULL AUTO_INCREMENT,
 	`route_name` varchar(255) NOT NULL,
 	`completion_time` DECIMAL(5,2) NOT NULL,
-	PRIMARY KEY (`route_id`)
+	`branch_id` INT NOT NULL,
+	PRIMARY KEY (`route_id`),
+	CONSTRAINT `routes_fk0` FOREIGN KEY (`branch_id`) REFERENCES `branches`(`branch_id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
