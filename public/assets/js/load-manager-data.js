eb5628bd181068a3d9fc6f404bab4e20610cac22
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2022-07-07 12:27:22 +0530

    add jquery

--- public/assets/js/load-manager-data.js
+++ public/assets/js/load-manager-data.js
@@ -1,8 +1,13 @@
 $(window).on("load", async function () {
   let element = $(".content");
+  $("#nav-ontheway-tab").trigger("click");
+  $("#nav-recieved-tab").trigger("click");
+  $("#nav-ontheway-tab").trigger("click");
+
   const branch = element.attr("id");
   const trips = await axios.get("/trains/trips/" + branch);
   const train_trips = trips.data;
+
   for (let i = 0; i < train_trips.length; i++) {
     let trip = train_trips[i];
     let element_id = "#" + trip.trip_id;
